4793aa7a4f9094b5cd5c72768ed691027f594afe
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-08-05 22:06:35 +0100

    Styled it further

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <h1><em>High Performance</em> Laravel</h1>
         <h2>Find, understand, and fix the <em>real</em> causes of performance issues with your Laravel applications</h2>
         <nav>
-          <ul>
+          <ul className="flex space-x-4 font-bold">
             <li>
               <Link href="/">Home</Link>
             </li>
@@ -26,8 +26,8 @@ export default function Home() {
             </li>
           </ul>
           <form action="/search" method="GET">
-            <input type="search" name="q" />
-            <button type="submit">Search</button>
+            <input type="search" name="q" placeholder="Search for..." className="p-2 rounded-lg ring-2 ring-gray-500 ring-inset" />
+            <button type="submit" className="bg-caribbean-green-600 hover:bg-caribbean-green-700 active:bg-caribbean-green-800 text-white rounded-lg p-2 ml-2">Search</button>
           </form>
         </nav>
       </header>
