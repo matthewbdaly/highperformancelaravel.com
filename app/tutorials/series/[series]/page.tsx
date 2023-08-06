ebb50509e6bcf5613e02ced7fc9fe0c315bac2ca
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-08-06 18:25:24 +0100

    Can now view individual series

---
+++ app/tutorials/series/[series]/page.tsx
@@ -0,0 +1,80 @@
+import slugify from "slugify";
+import { ReactElement } from "react";
+import { Metadata } from "next";
+import matter from "gray-matter";
+import fs from "fs";
+import path from "path";
+import { PostFileProps } from "@/lib/functions";
+import Card from "@/components/Card";
+import { getAllSeries, getAllTutorials } from "@/lib/functions";
+import NewsletterForm from "@/components/NewsletterForm";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+const getSeriesBySlug = async (slug: string): Promise<{
+  data: {
+    featured_image?: string,
+    featured_image_username?: string;
+    title: string;
+    description: string;
+  },
+}> => {
+  const filePath = path.join(process.cwd(), "content/series/", `${slug}.md`);
+  const source = fs.readFileSync(filePath);
+  const { data } = matter(source);
+  data.slug = slug;
+  const filePropsData = data as PostFileProps;
+  return {
+    data: filePropsData,
+  };
+};
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { series: string };
+}): Promise<Metadata> {
+  const { data } = await getSeriesBySlug(params.series);
+  return {
+    title: `${data.title} | High Performance Laravel`,
+    description: data.description,
+    openGraph: {
+      title: `${data.title} | High Performance Laravel`,
+      description: data.description
+    }
+  }
+}
+
+export default async function Page({
+  params,
+}: {
+  params: { series: string };
+}): Promise<ReactElement> {
+  const entries = getAllTutorials().filter(data => data.series && slugify(data.series.toLowerCase()) === params.series);
+  const { data } = await getSeriesBySlug(params.series);
+
+  return (
+    <main>
+      <Header />
+      <hr />
+      <section className="w-full flex flex-row flex-wrap pt-8">
+        {entries.map(entry => {
+          const { series, ...entryWithoutSeries } = entry;
+          return (
+            <Card key={entry.slug} href={`/tutorials/series/${entry.slug}`} {...entryWithoutSeries} />
+          );
+        })}
+      </section>
+      <hr />
+      <NewsletterForm />
+      <hr />
+      <Footer />
+    </main>
+  );
+}
+
+export function generateStaticParams(): Array<{series: string}> {
+  return getAllSeries().map(item => {
+    return { series: item.slug};
+  });
+}
--- lib/functions.ts
+++ lib/functions.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import slugify from "slugify";
 
 export interface PostFileProps {
   title: string;
@@ -13,6 +14,7 @@ export interface PostFileProps {
 };
 
 const seriesDirectory = path.join(process.cwd(), "content/series")
+const tutorialsDirectory = path.join(process.cwd(), "content/tutorials")
 
 export const getAllSeries = (): Array<{
   title: string;
@@ -29,3 +31,17 @@ export const getAllSeries = (): Array<{
       return ((data as unknown) as PostFileProps);
     });
 };
+
+export const getAllTutorials = (): Array<PostFileProps> => {
+  return fs.readdirSync(tutorialsDirectory)
+  .map((filePath: string): PostFileProps => {
+    const fullPath = path.join(process.cwd(), `content/tutorials/${filePath}`);
+    const source = fs.readFileSync(fullPath);
+    const { data } = matter(source)
+    data.slug = `${slugify(data.series.toLowerCase())}/${slugify(filePath.split('.')[0].toLowerCase())}`;
+    return ((data as unknown) as PostFileProps);
+    }).sort((a, b) => {
+      return new Date(b.date).valueOf() - new Date(a.date).valueOf();
+    })
+};
+
