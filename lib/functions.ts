bdf75c17f99e594d31619b15f617fcd9e6b9e223
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-08-06 17:03:00 +0100

    Added functions file

---
+++ lib/functions.ts
@@ -0,0 +1,9 @@
+export interface PostFileProps {
+  title: string;
+  description: string;
+  date: string;
+  series: string;
+  featured_image?: string;
+  featured_image_username?: string;
+  slug: string;
+};
