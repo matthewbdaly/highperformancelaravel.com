6c5c3b66d238d7c634378a7cb2348305556f2961
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-08-06 16:22:42 +0100

    Moved newsletter form to its own component
    Also cleaned up duplication

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
+import NewsletterForm from '@/components/NewsletterForm'
 import Sidebar from '@/components/Sidebar'
 
 export default function Home() {
@@ -28,16 +29,7 @@ export default function Home() {
         <Sidebar />
       </section>
       <hr />
-      <section className="newsletter">
-        <h3>Struggling to make your application performant?</h3>
-        <p>Never miss a tutorial! Sign up for the newsletter below.</p>
-        <form action="/subscribe" method="POST">
-          <input type="text" name="first_name" autoComplete="given-name" placeholder="Your given (or first) name" />
-          <input type="email" name="email" placeholder="Your email address" />
-          <button type="submit">Sign me up!</button>
-        </form>
-        <p>You can unsubscribe at any time, and your information will not be shared.</p>
-      </section>
+      <NewsletterForm />
       <hr />
       <Footer />
     </main>
