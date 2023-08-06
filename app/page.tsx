2ce66002a8dff7a8c0d220007ee584ea105ab3ff
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-08-06 16:10:27 +0100

    Extracted sidebar to its own component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Sidebar from '@/components/Sidebar'
 import Link from 'next/link'
 
 export default function Home() {
@@ -50,24 +50,7 @@ export default function Home() {
           <p>Take the first step towards optimizing your Laravel application today. Explore my blog and discover practical tips, expert insights, and actionable advice to unlock the true potential of your Laravel code. With High Performance Laravel as your guide, you&apos;ll be well-equipped to build lightning-fast Laravel applications that will impress your users and give your clients&apos; websites a competitive edge.</p>
           <h3>Optimize your Laravel applications&apos; performance today. Explore High Performance Laravel now.</h3>
         </div>
-        <aside>
-          <p>Do you struggle to make your Laravel applications perform well?</p>
-          <p>Do you find it hard to figure out what advice is and isn&apos;t helpful when optimising the performance of your applications?</p>
-          <p>Are you unsure where to begin analysing performance bottlenecks?</p>
-          <p>High Performance Laravel is here to help.</p>
-          <h4>Hello, I&apos;m Matthew Daly</h4>
-          <p>I&apos;m a professional web developer with over ten years of experience building high performance web applications with numerous frameworks and programming languages.</p>
-          <p>I&apos;ll teach you about the real reasons your web application is slow, and provide straightforward tips to resolve them. I&apos;ll help you to:</p>
-          <ul>
-            <li>Avoid common misconceptions about web application performance</li>
-            <li>Understand the real reasons for performance bottlenecks in most applications</li>
-            <li>Learn to apply profiling techniques to find performance issues</li>
-            <li>Resolve common performance issues</li>
-            <li>Optimise your server configuration to improve performance</li>
-            <li>Write more efficient code</li>
-            <li>Make your front end more efficient</li>
-          </ul>
-        </aside>
+        <Sidebar />
       </section>
       <hr />
       <section className="newsletter">
