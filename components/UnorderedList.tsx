ca2732b1451199ae6bf92396f440f6d53eb91a89
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-08-06 20:08:27 +0100

    Amended unordered list component

--- components/UnorderedList.tsx
+++ components/UnorderedList.tsx
@@ -2,7 +2,7 @@ import React, { HTMLAttributes, ReactElement } from "react"
 
 const UnorderedList = ({ children, ...rest }: HTMLAttributes<HTMLUListElement>): ReactElement => {
   return (
-    <ul {...rest}>
+    <ul className="pl-4 list-disc" {...rest}>
       {children}
     </ul>
   )
