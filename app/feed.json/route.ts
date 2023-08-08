e3170b90b28784a8c5d4e48e5d2154cd9177a2cf
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-08-08 21:13:01 +0100

    Removed request object from RSS feed route handlers
    Not needed and may prevent caching when not statically generated. Don't
    plan for this site to not be static, but never know, so best to resolve
    it now.

--- app/feed.json/route.ts
+++ app/feed.json/route.ts
@@ -1,6 +1,6 @@
 import { getFeed } from "@/lib/functions"
 
-export async function GET(req: Request): Promise<Response> {
+export async function GET(): Promise<Response> {
   const feed = getFeed();
   return new Response(feed.json1(), { headers: { "content-type": "application/feed+json" } })
 }
--- app/rss.xml/route.ts
+++ app/rss.xml/route.ts
@@ -1,6 +1,6 @@
 import { getFeed } from "@/lib/functions"
 
-export async function GET(req: Request): Promise<Response> {
+export async function GET(): Promise<Response> {
   const feed = getFeed();
   return new Response(feed.rss2(), { headers: { "content-type": "application/rss+xml" } })
 }
