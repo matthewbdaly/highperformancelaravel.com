19a57aa89c6acc95846ee103dac9afe8e68c097b
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-08-08 22:08:08 +0100

    Fixed a Typescript issue

--- components/Search.tsx
+++ components/Search.tsx
@@ -3,8 +3,15 @@
 import { ReactElement, useEffect, useState } from "react";
 import Fuse from "fuse.js";
 
+interface SearchItem {
+  title: string;
+  description: string;
+  series: string;
+  link: string;
+}
+
 export default function Search(): ReactElement {
-  const [searchResults, setSearchResults] = useState([]);
+  const [searchResults, setSearchResults] = useState<Fuse.FuseResult<SearchItem>[]>([]);
   const fuse = new Fuse([], { keys: ["title", "description", "series"], shouldSort: true });
 
   useEffect(() => {
@@ -29,13 +36,12 @@ export default function Search(): ReactElement {
       <input type="search" name="q" placeholder="Search for..." className="p-2 rounded-lg ring-2 ring-gray-500 ring-inset" onChange={handleSearch} />
       <button type="submit" className="bg-caribbean-green-600 hover:bg-caribbean-green-700 active:bg-caribbean-green-800 text-white rounded-lg p-2 ml-2">Search</button>
       {searchResults.length > 0 && (
-  <ul>
-    {searchResults.map(result => (
-      <a href={result.item.link} key={result.item.link}><li>{result.item.title}</li></a>
-    ))}
-  </ul>
-)}
-
+        <ul>
+          {searchResults.map(result => (
+            <a href={result.item.link} key={result.item.link}><li>{result.item.title}</li></a>
+          ))}
+        </ul>
+      )}
     </form>
   )
 }
