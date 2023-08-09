dd2d6f8f31d8926339454c642e2f7620e1e4914a
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-08-09 22:17:49 +0100

    Now have a reasonably presentable mobile menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,46 @@
-import { ReactElement } from "react";
-import Link from 'next/link'
+"use client";
+
+import Link from "next/link";
+import { ReactElement, useState } from "react";
 import Search from "./Search";
 
 export default function Header(): ReactElement {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
   return (
     <header>
-      <h1><Link href="/"><em>High Performance</em> Laravel</Link></h1>
-      <h2>Find, understand, and fix the <em>real</em> causes of performance issues with your Laravel applications</h2>
-      <nav>
+      <div className="flex items-center justify-between">
+        <h1><Link href="/"><em>High Performance</em> Laravel</Link></h1>
+        <button
+          className="block md:hidden"
+          onClick={() => setMenuOpen(!isMenuOpen)}
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            {isMenuOpen ? (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            ) : (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            )}
+          </svg>
+        </button>
+      </div>
+      <nav className={`${isMenuOpen ? "block" : "hidden"} md:block`}>
         <ul>
           <li>
             <Link href="/">Home</Link>
