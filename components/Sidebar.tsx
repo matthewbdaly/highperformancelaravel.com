9b93fa34835067c511864af8a3e773f0167fe731
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-08-12 17:26:41 +0100

    Amended the sidebar a bit

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,22 +3,23 @@ import { ReactElement } from "react";
 export default function Sidebar(): ReactElement {
   return (
     <aside>
-      <p>Do you struggle to make your Laravel applications perform well?</p>
-      <p>Do you find it hard to figure out what advice is and isn&apos;t helpful when optimising the performance of your applications?</p>
-      <p>Are you unsure where to begin analysing performance bottlenecks?</p>
-      <p>High Performance Laravel is here to help.</p>
-      <h4>Hello, I&apos;m Matthew Daly</h4>
-      <p>I&apos;m a professional web developer with over ten years of experience building high performance web applications with numerous frameworks and programming languages.</p>
-      <p>I&apos;ll teach you about the real reasons your web application is slow, and provide straightforward tips to resolve them. I&apos;ll help you to:</p>
-      <ul>
-        <li>Avoid common misconceptions about web application performance</li>
-        <li>Understand the real reasons for performance bottlenecks in most applications</li>
-        <li>Learn to apply profiling techniques to find performance issues</li>
-        <li>Resolve common performance issues</li>
-        <li>Optimise your server configuration to improve performance</li>
-        <li>Write more efficient code</li>
-        <li>Make your front end more efficient</li>
-      </ul>
+      <section>
+        <p>Do you struggle to make your Laravel applications perform well?</p>
+        <p>Do you find it hard to figure out what advice is and isn&apos;t helpful when optimising the performance of your applications?</p>
+        <p>Are you unsure where to begin analysing performance bottlenecks?</p>
+        <p>High Performance Laravel is here to help.</p>
+        <h4>Hello, I&apos;m Matthew Daly</h4>
+        <p>I&apos;m a professional web developer with over ten years of experience building high performance web applications with numerous frameworks and programming languages.</p>
+        <p>I&apos;ll teach you about the <em>real</em> reasons your web application is slow, and provide straightforward tips to resolve them. With my help youI&apos;ll be able to:</p>
+        <ul>
+          <li>Avoid common misconceptions about web application performance</li>
+          <li>Understand the real reasons for performance bottlenecks in most applications</li>
+          <li>Learn to apply profiling techniques to find performance issues</li>
+          <li>Resolve common performance issues</li>
+          <li>Optimise your server configuration to improve performance</li>
+          <li>Write more efficient code</li>
+        </ul>
+      </section>
     </aside>
   );
 }
