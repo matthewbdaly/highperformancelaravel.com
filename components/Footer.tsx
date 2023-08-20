f129c13fc940416de57ad4953428399ac0c46037
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-08-20 21:00:02 +0100

    Amended footer a little more

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,16 +5,24 @@ export default function Footer(): ReactElement {
   return (
     <footer>
       <section>
-      <div className="socialIcons">
-        <a href="https://matthewdaly.co.uk" target="_blank" rel="noopener noreferrer" title="Homepage"><i className="icon-house"></i></a>
-        <a href="https://github.com/matthewbdaly" target="_blank" rel="noopener noreferrer" title="Github"><i className="icon-github"></i></a>
-        <a href="https://mstdn.social/@matthewbdaly" target="_blank" rel="noopener noreferrer" title="Mastodon"><i className="icon-mastodon"></i></a>
-        <a href="https://stackoverflow.com/users/63717/matthew-daly" target="_blank" rel="noopener noreferrer" title="Stack Overflow"><i className="icon-stack-overflow"></i></a>
-        <a href="https://dev.to/matthewbdaly" target="_blank" rel="noopener noreferrer" title="Dev.to"><i className="icon-dev"></i></a>
-      </div>
+        <div>
+          <p><Link href="/privacy-policy">Privacy policy</Link></p>
+        </div>
+        <div>
+        </div>
+        <div>
+          <div className="socialIcons">
+            <a href="https://matthewdaly.co.uk" target="_blank" rel="noopener noreferrer" title="Homepage"><i className="icon-house"></i></a>
+            <a href="https://github.com/matthewbdaly" target="_blank" rel="noopener noreferrer" title="Github"><i className="icon-github"></i></a>
+            <a href="https://mstdn.social/@matthewbdaly" target="_blank" rel="noopener noreferrer" title="Mastodon"><i className="icon-mastodon"></i></a>
+            <a href="https://stackoverflow.com/users/63717/matthew-daly" target="_blank" rel="noopener noreferrer" title="Stack Overflow"><i className="icon-stack-overflow"></i></a>
+            <a href="https://dev.to/matthewbdaly" target="_blank" rel="noopener noreferrer" title="Dev.to"><i className="icon-dev"></i></a>
+          </div>
+        </div>
       </section>
-      <p><Link href="/privacy-policy">Privacy policy</Link></p>
-      <p>Copyright &copy; Matthew Daly {new Date().getFullYear()}</p>
+      <div>
+        <p>Copyright &copy; Matthew Daly {new Date().getFullYear()}</p>
+      </div>
     </footer>
   );
 }
