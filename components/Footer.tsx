793a4d68be2f9a566427426c00adbb044dd7c226
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-08-20 17:50:58 +0100

    Styled footer a bit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 import { ReactElement } from "react";
 
 export default function Footer(): ReactElement {
   return (
     <footer>
+      <p><Link href="/privacy-policy">Privacy policy</Link></p>
       <div>
         <p>Made with ❤️ by <a href="https://matthewdaly.co.uk">Matthew Daly</a></p>
       </div>
