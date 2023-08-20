c476075132952fba98bcceb7ab2841979eede329
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-08-20 17:55:34 +0100

    Moved social icons to footer

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -22,13 +22,6 @@ export default function Sidebar(): ReactElement {
           <li>Optimise your server configuration to improve performance</li>
           <li>Write more efficient code</li>
         </ul>
-          <div className="socialIcons">
-            <a href="https://matthewdaly.co.uk" target="_blank" rel="noopener noreferrer" title="Homepage"><i className="icon-house"></i></a>
-            <a href="https://github.com/matthewbdaly" target="_blank" rel="noopener noreferrer" title="Github"><i className="icon-github"></i></a>
-            <a href="https://mstdn.social/@matthewbdaly" target="_blank" rel="noopener noreferrer" title="Mastodon"><i className="icon-mastodon"></i></a>
-            <a href="https://stackoverflow.com/users/63717/matthew-daly" target="_blank" rel="noopener noreferrer" title="Stack Overflow"><i className="icon-stack-overflow"></i></a>
-            <a href="https://dev.to/matthewbdaly" target="_blank" rel="noopener noreferrer" title="Dev.to"><i className="icon-dev"></i></a>
-          </div>
       </section>
     </aside>
   );
