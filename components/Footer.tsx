96fcc2584b625d451ee5f020d382219e9f6da910
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-08-20 20:39:06 +0100

    Some progress updating the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,6 +4,7 @@ import { ReactElement } from "react";
 export default function Footer(): ReactElement {
   return (
     <footer>
+      <section>
       <div className="socialIcons">
         <a href="https://matthewdaly.co.uk" target="_blank" rel="noopener noreferrer" title="Homepage"><i className="icon-house"></i></a>
         <a href="https://github.com/matthewbdaly" target="_blank" rel="noopener noreferrer" title="Github"><i className="icon-github"></i></a>
@@ -11,10 +12,8 @@ export default function Footer(): ReactElement {
         <a href="https://stackoverflow.com/users/63717/matthew-daly" target="_blank" rel="noopener noreferrer" title="Stack Overflow"><i className="icon-stack-overflow"></i></a>
         <a href="https://dev.to/matthewbdaly" target="_blank" rel="noopener noreferrer" title="Dev.to"><i className="icon-dev"></i></a>
       </div>
+      </section>
       <p><Link href="/privacy-policy">Privacy policy</Link></p>
-      <div>
-        <p>Made with ❤️ by <a href="https://matthewdaly.co.uk">Matthew Daly</a></p>
-      </div>
       <p>Copyright &copy; Matthew Daly {new Date().getFullYear()}</p>
     </footer>
   );
