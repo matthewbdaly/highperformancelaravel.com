73f8e9d8614b94f822cf23c31fbb330f75950002
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-08-23 22:05:08 +0100

    Is now better aligned

--- components/Search.tsx
+++ components/Search.tsx
@@ -33,13 +33,13 @@ export default function Search(): ReactElement {
   };
 
   return (
-    <form className="w-full md:w-1/4 lg:w-1/3" autoComplete="off" onSubmit={(e: FormEvent) => e.preventDefault()}>
+    <form className="w-full md:w-1/4 lg:w-1/3 relative inline-block" autoComplete="off" onSubmit={(e: FormEvent) => e.preventDefault()}>
       <input type="search" name="q" placeholder="Search for..." className="p-2 rounded-lg shadow-lg ring-1 ring-gray-200 ring-inset outline-1 outline-gray-300 w-full text-gray-600" onChange={handleSearch} />
       {searchResults.length > 0 && (
-        <ul className="space-x-0 z-10 absolute bg-transparent grid grid-cols-1 mr-4 md:mr-0">
+        <ul className="space-x-0 z-10 absolute bg-transparent grid grid-cols-1 mr-4 md:mr-0 top-full left-0 right-0">
           {searchResults.map(result => (
             <a href={result.item.link} key={result.item.link}>
-              <li className="border-2 border-gray-300 p-2 block text-gray-600 bg-gray-50 w-full">{result.item.title}</li>
+              <li className="border-2 border-gray-300 p-2 block text-gray-600 bg-gray-50 w-full hover:bg-gray-200">{result.item.title}</li>
             </a>
           ))}
         </ul>
