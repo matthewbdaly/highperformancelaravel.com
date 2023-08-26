851f4f89144396b2059c48b7de8bd998eab18d0d
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-08-26 17:32:18 +0100

    Fixed an accessibility issue

--- app/page.tsx
+++ app/page.tsx
@@ -5,28 +5,28 @@ export default function Home() {
   return (
     <section className="content-wrapper">
       <div className="content">
-        <h3>Are you struggling with the performance of your Laravel application?</h3>
+        <h2>Are you struggling with the performance of your Laravel application?</h2>
         <p>Do you see consistent performance issues you don&apos;t understand?</p>
         <p>Are you unsure how to resolve them?</p>
         <p>Not sure if choosing Laravel for a new project is the right choice?</p>
         <p><strong>High Performance Laravel</strong> is here to help</p>
         <blockquote>
-          <h4>Unlock the full potential of your Laravel code.</h4>
+          <h3>Unlock the full potential of your Laravel code.</h3>
           <p>When you first start working with Laravel, it can be hard to understand the real reasons for slow application performance. There&apos;s a lot of misinformation around, and claims that Laravel or PHP are inherently slow, which just isn&apos;t the case, but are hard to debunk when you first start out.</p>
           <p>As a Laravel developer, I know the <strong>real</strong> pain points and common issues that can slow down your Laravel application. With High Performance Laravel, I&apos;ll share my best practices and insights to help you resolve common performance bottlenecks in your application.</p>
         </blockquote>
-        <h2>Current articles...</h2>
-        <h3>
+        <h4>Current articles...</h4>
+        <h5>
           <Link href="/tutorials/series/myth-busters"><em>Avoid falling for performance myths</em></Link>
-        </h3>
+        </h5>
         <p>Don&apos;t waste time on dealing with performance myths that won&apos;t help you make your site faster and more efficient</p>
-        <h3>
+        <h5>
           <Link href="/tutorials/series/server-configuration"><em>Configure your server for better performance</em></Link>
-        </h3>
+        </h5>
         <p>Learn how to configure your production server to get the best performance from your Laravel application</p>
-        <h3>
+        <h5>
           <Link href="/tutorials/series/writing-efficient-applications"><em>Write efficient applications</em></Link>
-        </h3>
+        </h5>
         <p>Make sure your Laravel application is performant and efficient by learning what the most likely bottlenecks are and avoiding them</p>
       </div>
       <Sidebar />
