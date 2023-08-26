aae128534e9d954ae56db292ef784526258ce05b
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-08-26 17:11:28 +0100

    Finished home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,33 @@
 import Sidebar from "@/components/Sidebar";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <section className="content-wrapper">
       <div className="content">
-        <h2>Cut through the disinformation, and learn <em>real</em> techniques to optimize your Laravel applications</h2>
-        <p>Struggling to get the most out of your Laravel applications?</p>
+        <h3>Are you struggling with the performance of your Laravel application?</h3>
+        <p>Do you see consistent performance issues you don&apos;t understand?</p>
+        <p>Are you unsure how to resolve them?</p>
+        <p>Not sure if choosing Laravel for a new project is the right choice?</p>
+        <p><strong>High Performance Laravel</strong> is here to help</p>
         <blockquote>
-          <h3>Is your Laravel application running slower than you&apos;d like?</h3>
-          <p>I understand how frustrating it can be to deal with a sluggish Laravel application. That&apos;s why I&apos;ve created High Performance Laravel – a resource dedicated to providing you with practical, informative, and approachable tips that will help you optimize your Laravel applications for maximum performance.</p>
           <h4>Unlock the full potential of your Laravel code.</h4>
-          <p>As a fellow developer, I know the pain points and common issues that can slow down your Laravel application. Through High Performance Laravel, I offer clear solutions and actionable advice to help you optimize your code, boost performance, and leave your competition behind.</p>
+          <p>When you first start working with Laravel, it can be hard to understand the real reasons for slow application performance. There&apos;s a lot of misinformation around, and claims that Laravel or PHP are inherently slow, which just isn&apos;t the case, but are hard to debunk when you first start out.</p>
+          <p>As a Laravel developer, I know the <strong>real</strong> pain points and common issues that can slow down your Laravel application. With High Performance Laravel, I&apos;ll share my best practices and insights to help you resolve common performance bottlenecks in your application.</p>
         </blockquote>
-        <h4>Stay ahead of the game with performance optimization tips.</h4>
-        <p>In today&apos;s fast-paced digital world, performance matters. Slow-loading websites and applications can drive users away and harm your clients&apos; business. That&apos;s why I&apos;ve created a wide range of articles and tutorials on performance optimization, including database optimization, caching strategies, query optimization, and more. By breaking down complex concepts into digestible steps, I make it easy for you to implement these optimizations and improve your app&apos;s speed and efficiency.</p>
-        <h4>Level up your skills and optimize your Laravel applications.</h4>
-        <p>My goal is to empower you to become a performance optimization expert. Whether you&apos;re a beginner or an experienced Laravel developer, High Performance Laravel offers valuable insights, best practices, and real-world examples that will help you unlock the full potential of your Laravel code. From optimizing your database queries to implementing caching mechanisms, you&apos;ll find actionable steps to improve your application&apos;s performance.</p>
-        <h4>Ready to supercharge your Laravel application?</h4>
-        <p>Take the first step towards optimizing your Laravel application today. Explore my blog and discover practical tips, expert insights, and actionable advice to unlock the true potential of your Laravel code. With High Performance Laravel as your guide, you&apos;ll be well-equipped to build lightning-fast Laravel applications that will impress your users and give your clients&apos; websites a competitive edge.</p>
-        <h3>Optimize your Laravel applications&apos; performance today. Explore High Performance Laravel now.</h3>
+        <h2>Current articles...</h2>
+        <h3>
+          <Link href="/tutorials/series/myth-busters"><em>Avoid falling for performance myths</em></Link>
+        </h3>
+        <p>Don&apos;t waste time on dealing with performance myths that won&apos;t help you make your site faster and more efficient</p>
+        <h3>
+          <Link href="/tutorials/series/server-configuration"><em>Configure your server for better performance</em></Link>
+        </h3>
+        <p>Learn how to configure your production server to get the best performance from your Laravel application</p>
+        <h3>
+          <Link href="/tutorials/series/writing-efficient-applications"><em>Write efficient applications</em></Link>
+        </h3>
+        <p>Make sure your Laravel application is performant and efficient by learning what the most likely bottlenecks are and avoiding them</p>
       </div>
       <Sidebar />
     </section>
