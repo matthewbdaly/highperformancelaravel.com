4d592c5ec371c99c5dacd41df88ad9440bca08c0
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-09-03 22:11:50 +0100

    Added a custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import { ReactElement } from 'react';
+
+export default function NotFound(): ReactElement {
+  return (
+    <section className="content-wrapper">
+      <div className="content">
+        <p>Sorry, that page doesn&apos;t exist.</p>
+        <p>You may want to try one of the following instead:</p>
+        <ul className="fallback-links">
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/all-tutorials/1">Tutorials</Link></li>
+        </ul>
+      </div>
+    </section>
+  );
+}
