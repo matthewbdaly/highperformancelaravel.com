f6e2388746a61f231faedc9f94ffc5c64bc05099
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-09-10 17:51:12 +0100

    Added attribution to hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { FC, ReactElement } from "react";
-import UnsplashImage from "./UnsplashImage";
+import { getUnsplashPhoto } from "@/lib/functions";
 
 interface Props {
   featured_image_id: string,
@@ -8,22 +8,27 @@ interface Props {
   description: string
 };
 
-const Hero: FC<Props> = ({ featured_image_id, title, description }): ReactElement => {
+const Hero: FC<Props> = async ({ featured_image_id, title, description }): Promise<ReactElement> => {
+  const featuredImage = await getUnsplashPhoto(featured_image_id);
+
   return (
     <div className="heroWrapper">
-      {featured_image_id &&
-      <UnsplashImage
-        type="full"
-        imageId={featured_image_id}
-        classes="object-cover object-center"
+      {featuredImage &&
+      <Image
+        src={featuredImage.urls.full}
+        alt={featuredImage.alt_description || ""}
+        blurDataURL={featuredImage.blur_hash || undefined}
+        className="object-cover object-center"
         fill={true}
         priority={true}
         sizes="800px"
+        unoptimized
       />
       }
       <div className="heroContent">
         <h2 className="text-2xl font-bold py-2 px-16 my-2">{title}</h2>
         <h3 className="text-xl py-2 px-16 my-2">{description}</h3>
+        <p className="block absolute bottom-2 text-xs font-light">Photo by <a href={`https://unsplash.com/@${featuredImage.user.username}?utm_source=highperformancelaravel&utm_medium=referral`}>{featuredImage.user.name}</a> on <a href="https://unsplash.com/?utm_source=highperformancelaravel&utm_medium=referral">Unsplash</a></p>
       </div>
     </div>
   );
