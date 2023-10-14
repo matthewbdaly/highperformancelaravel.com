8050c23c3962c898f6e23c9a28e715a58ad83364
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-10-14 21:16:51 +0100

    Amended indentation and added an editorconfig file

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,48 +6,52 @@ import Footer from '@/components/Footer';
 
 const baseUrl = process.env.URL ? new URL(process.env.URL) : new URL(`http://localhost:${process.env.PORT || 3000}`);
 
-  export const metadata: Metadata = {
+export const metadata: Metadata = {
   metadataBase: baseUrl,
-  title: `High Performance Laravel`,
-  description: `Learn how to optimize your Laravel application for high performance, and avoid wasting time on pointless performance myths`,
-    robots: {
-      index: true
-    },
-    twitter: {
-      card: `summary_large_image`
-    },
-    openGraph: {
-      title: `High Performance Laravel`,
-      description: `Learn how to optimize your Laravel application for high performance, and avoid wasting time on pointless performance myths`,
+  title: {
+    default: `High Performance Laravel`,
+  },
+  description: {
+    default: `Learn how to optimize your Laravel application for high performance, and avoid wasting time on pointless performance myths`,
+  },
+  robots: {
+    index: true
+  },
+  twitter: {
+    card: `summary_large_image`
+  },
+  openGraph: {
+    title: `High Performance Laravel`,
+    description: `Learn how to optimize your Laravel application for high performance, and avoid wasting time on pointless performance myths`,
       locale: `en`,
-      type: 'website',
-      url: baseUrl,
-      images: [
-        {
-          url: `/logo.png`,
-          width: 32,
-          height: 32,
-          alt: 'High Performance Laravel',
-          type: 'image/png'
-        }
-      ]
+    type: 'website',
+    url: baseUrl,
+    images: [
+      {
+        url: `/logo.png`,
+        width: 32,
+        height: 32,
+        alt: 'High Performance Laravel',
+        type: 'image/png'
+      }
+    ]
+  },
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#3b0764' },
+    { media: '(prefers-color-scheme: dark)', color: '#3b0764' },
+  ],
+  alternates: {
+    canonical: baseUrl,
+    languages: {
+      'en': baseUrl
     },
-    themeColor: [
-      { media: '(prefers-color-scheme: light)', color: '#3b0764' },
-      { media: '(prefers-color-scheme: dark)', color: '#3b0764' },
-    ],
-    alternates: {
-      canonical: baseUrl,
-      languages: {
-        'en': baseUrl
-      },
-      types: {
-        'application/rss+xml': `${baseUrl}rss.xml`,
-        'application/atom+xml': `${baseUrl}atom.xml`,
-        'application/json': `${baseUrl}feed.json`
-      },
+    types: {
+      'application/rss+xml': `${baseUrl}rss.xml`,
+      'application/atom+xml': `${baseUrl}atom.xml`,
+      'application/json': `${baseUrl}feed.json`
     },
-}
+  },
+};
 
 export default function RootLayout({
   children,
