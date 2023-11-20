04e173298b276c81dfc28ca0ed68154d128cf127
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2023-11-20 22:16:47 +0000

    Static export now seems to work

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from "react";
-import Image from "next/image";
+import ExportedImage from "next-image-export-optimizer";
 import profilePic from "../public/img/profile.png";
 
 export default function Sidebar(): ReactElement {
@@ -10,7 +10,7 @@ export default function Sidebar(): ReactElement {
         <p>Do you find it hard to figure out what advice is and isn&apos;t helpful when optimising the performance of your applications?</p>
         <p>Are you unsure where to begin analysing performance bottlenecks?</p>
         <p>High Performance Laravel is here to help.</p>
-        <Image src={profilePic} alt="Picture of the author" className="rounded-full border-2 border-gray-50 bg-gray-200 mx-auto" height={200} width={200} placeholder="blur" />
+        <ExportedImage src={profilePic} alt="Picture of the author" className="rounded-full border-2 border-gray-50 bg-gray-200 mx-auto" height={200} width={200} placeholder="blur" />
         <h4>Hello, I&apos;m Matthew Daly</h4>
         <p>I&apos;m a professional web developer with over ten years of experience building high performance web applications with numerous frameworks and programming languages.</p>
         <p>I&apos;ll teach you about the <em>real</em> reasons your web application is slow, and provide straightforward tips to resolve them. With my help you&apos;ll be able to:</p>
--- next.config.js
+++ next.config.js
@@ -10,13 +10,28 @@ const nextConfig = {
     typedRoutes: true
   },
   images: {
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com'
       }
     ]
-  }
-}
+  },
+  transpilePackages: ["next-image-export-optimizer"],
+  env: {
+    nextImageExportOptimizer_imageFolderPath: "public/img",
+    nextImageExportOptimizer_exportFolderPath: "out",
+    nextImageExportOptimizer_quality: "75",
+    nextImageExportOptimizer_storePicturesInWEBP: "true",
+    nextImageExportOptimizer_exportFolderName: "nextImageExportOptimizer",
+
+    // If you do not want to use blurry placeholder images, then you can set
+    // nextImageExportOptimizer_generateAndUseBlurImages to false and pass
+    // `placeholder="empty"` to all <ExportedImage> components.
+    nextImageExportOptimizer_generateAndUseBlurImages: "true",
+  },
+};
 
 module.exports = nextConfig
