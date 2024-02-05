d2c0218652b2c1d8fd10c3140f02d15925e8970c
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2024-02-05 21:05:18 +0000

    Integrated Formspree

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,54 +1,43 @@
 "use client";
-import { FormEvent, ReactElement, useState } from "react";
+
+import { FormEvent, ReactElement } from "react";
+import { useForm, ValidationError } from "@formspree/react";
+
 
 export default function ContactForm(): ReactElement {
-  const [submitted, setSubmitted] = useState(false);
-  const [error, setError] = useState("");
-  const validateSubmit = (event: FormEvent) => {
-    event.preventDefault();
+  const [state, handleSubmit] = useForm(process.env.NEXT_PUBLIC_FORM);
 
-    const myForm = event.target as HTMLFormElement;
-    const formData = new FormData(myForm);
-
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData as any).toString(),
-    })
-    .then(() => setSubmitted(true))
-    .catch(() => setError("Sorry, something went wrong"));
-  };
+  if (state.succeeded) {
+      return (<p className="bg-purple-200 p-2 my-2">Thank you for your submission</p>);
+  }
 
   return (
-    <form name="contact" method="POST" netlify-honeypot="employer-name" data-netlify="true" data-netlify-recaptcha="true" className="my-4" onSubmit={validateSubmit}>
-      <input type="hidden" name="form-name" value="contact" />
-      {submitted && <p className="bg-purple-200 p-2 my-2">Thank you for your submission</p>}
-      <p className="hidden">
-        <label>Employer Name: <input type="text" name="employer-name" /></label>
-      </p>
+    <form name="contact" method="POST" className="my-4" onSubmit={handleSubmit}>
       <div className="mb-4">
         <label className="block text-sm font-bold mb-2" htmlFor="name">
           Your Name
         </label>
-        <input className="shadow appearance-none border rounded w-full md:w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" name="name" type="text" placeholder="Your Name" disabled={submitted} />
+        <input className="shadow appearance-none border rounded w-full md:w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" name="name" type="text" placeholder="Your Name" disabled={state.submitting} />
+        <ValidationError prefix="Name" field="name" errors={state.errors} />
       </div>
       <div className="mb-4">
         <label className="block text-sm font-bold mb-2" htmlFor="email">
           Your Email
         </label>
-        <input className="shadow appearance-none border rounded w-full md:w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" name="email" type="email" placeholder="Your Email" disabled={submitted} />
+        <input className="shadow appearance-none border rounded w-full md:w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" name="email" type="email" placeholder="Your Email" disabled={state.submitting} />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
       </div>
       <div className="mb-4">
         <label className="block text-sm font-bold mb-2" htmlFor="message">
           Message
         </label>
-        <textarea className="shadow appearance-none border rounded w-full md:w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-y" id="message" name="message" placeholder="Your Message" rows={3} disabled={submitted} />
+        <textarea className="shadow appearance-none border rounded w-full md:w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-y" id="message" name="message" placeholder="Your Message" rows={3} disabled={state.submitting} />
+        <ValidationError prefix="Message" field="message" errors={state.errors} />
       </div>
-      <div data-netlify-recaptcha="true"></div>
       <p>
-        <button type="submit" className="bg-gradient-to-r from-purple-400 to-purple-600 text-white rounded-lg shadow-lg p-2 px-4 w-full md:w-1/2"  disabled={submitted}>Send</button>
+        <button type="submit" className="bg-gradient-to-r from-purple-400 to-purple-600 text-white rounded-lg shadow-lg p-2 px-4 w-full md:w-1/2"  disabled={state.submitting}>Send</button>
       </p>
-      {error && <p>{error}</p>}
+      <ValidationError errors={state.errors} />
     </form>
   );
 }
